3c36f5d113e6973341d3e3f16106f6a0b371800d
Rodrigo Peniche <rodrigopeniche@Rodrigos-MacBook-Pro.local>
2019-05-11 20:22:16 -0500

    Added record deletions and fixed existing records not showing up. Fixed database integrity bugs

--- admin2/siadesod/models.py
+++ admin2/siadesod/models.py
@@ -18,8 +18,6 @@ class User(models.Model):
     admin = models.BooleanField(default = False)
 
 class Staff(User):
-    #staff = models.OneToOneField(User, on_delete=models.DO_NOTHING, parent_link=True)
-    #staff_id = models.ForeignKey(User, on_delete='DO_NOTHING', on_update='DO_NOTHING', unique=True)
     salary = models.IntegerField(default=0)
 
     TYPE_CHOICE = (
@@ -29,51 +27,23 @@ class Staff(User):
 
     staff_type = models.CharField(max_length = 1, choices = TYPE_CHOICE)
 
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
-
-    def __str__(self):
-        return self.toJSON()
-        #return str(self.user_id) + ", " + self.first_name + ", " + self.last_name + ", " + self.email + ", " + str(self.date_of_birth) + ", " + self.phone + ", " + self.address + ", " + str(self.salary) + ", " + self.staff_type
 
 class Patient(User):
-    #patient = models.OneToOneField(User, on_delete=models.DO_NOTHING, parent_link=True)
-    #patiend_id = models.ForeignKey(User, on_delete='DO_NOTHING', on_update='DO_NOTHING', unique=True)
     curp = models.CharField(max_length = 18, unique=True)
     insurance = models.CharField(max_length = 45)
     allergies = models.CharField(max_length = 200)
 
-    '''def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
-
-    def __str__(self):
-        return self.toJSON()'''
-        #return str(self.user_id) + ", " + self.first_name + ", " + self.last_name + ", " + self.email + ", " + str(self.date_of_birth) + ", " + self.phone + ", " + self.address + ", " + self.curp + ", " + self.insurance + ", " + self.allergies
-
 
 class Dentist(User):
-    #dentist = models.OneToOneField(User, on_delete=models.DO_NOTHING, parent_link=True)
-    #dentist_id = models.ForeignKey(User, on_delete='DO_NOTHING', on_update='DO_NOTHING', unique=True)
     hourly_rate = models.IntegerField(default=0)
     cedula = models.CharField(max_length = 45, unique=True)
     specialty = models.CharField(max_length = 45)
-
-    '''def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
-
-    def __str__(self):
-        return self.toJSON()'''
-        #return str(self.user_id) + ", " + self.first_name + ", " + self.last_name + ", " + self.email + ", " + str(self.date_of_birth) + ", " + self.phone + ", " + self.address + ", " + str(self.rate) + ", " + self.cedula + ", " + self.specialty
-
-    
+   
 
 class Appointment(models.Model):
     appointment_id = models.IntegerField(primary_key=True)
-    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
-    dentist = models.ForeignKey(Dentist, on_delete=models.DO_NOTHING)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    dentist = models.ForeignKey(Dentist, on_delete=models.CASCADE)
 
     date = models.DateField(null=True)
 
@@ -86,27 +56,13 @@ class Appointment(models.Model):
     status = models.CharField(max_length = 1, choices = STATUS_CHOICE)
     description = models.CharField(max_length = 45, default = 'Ninguno')
 
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
-
-    def __str__(self):
-        return self.toJSON()
-        #return str(self.appointment_id) + ", " + str(self.patient) + ", " + str(self.dentist) + ", " + str(self.date) + ", " + self.status + "," + self.description
 
 class Treatment(models.Model):
     treatment_id = models.IntegerField(primary_key=True)
-    appointment = models.ForeignKey(Appointment, on_delete=models.DO_NOTHING)
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
     start_date = models.DateField(null=True)
     end_date = models.DateField(null=True)
     diagnostic = models.CharField(max_length = 100, default = 'Ninguno')
     medicine = models.CharField(max_length = 45, default = 'Ninguno')
     observations = models.CharField(max_length = 100)
 
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
-
-    def __str__(self):
-        return self.toJSON()
-        #return str(self.treatment_id) + ", " + str(self.appointment) + ", " + str(self.start_date) + ", " + str(self.end_date) + ", " + self.diagnostic + ", " + self.medicine + ", " + self.observations 
--- admin2/siadesod/views.py
+++ admin2/siadesod/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from .models import Patient, Dentist, Staff, Appointment, Treatment
+from django.db import IntegrityError
 
 # Create your views here.
 from django.http import HttpResponse
@@ -11,22 +12,32 @@ def index(request):
 
 def all_patients(request):
     if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        address = request.POST.get('address')
-        dob = request.POST.get('dob')
-        insurance = request.POST.get('insurance')
-        phone = request.POST.get('phone')
-        email = request.POST.get('email')
-        curp = request.POST.get('curp')
-        allergies = request.POST.get('allergies')
+        if 'first_name' in request.POST:
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            address = request.POST.get('address')
+            dob = request.POST.get('dob')
+            insurance = request.POST.get('insurance')
+            phone = request.POST.get('phone')
+            email = request.POST.get('email')
+            curp = request.POST.get('curp')
+            allergies = request.POST.get('allergies')
 
-        latest_patient = Patient.objects.latest('user_id')
-        latest_patient_id = latest_patient.user_id
+            try:
+                latest_patient = Patient.objects.latest('user_id')
+                latest_patient_id = latest_patient.user_id
+            except:
+                latest_patient_id = 0
         
-        patient = Patient((latest_patient_id + 1), first_name, last_name, email, dob, phone, address, False, curp, insurance, allergies)
-        patient.save()
-        
+            patient = Patient((latest_patient_id + 1), first_name, last_name, email, dob, phone, address, False, curp, insurance, allergies)
+            patient.save()
+
+        if 'delete' in request.POST:
+            try:
+                Patient.objects.filter(pk=request.POST.get('delete')).delete()
+            except:
+                print('Error bitch')
+
     patients_list = Patient.objects.all()
     template = loader.get_template('patients.html')
     context = {
@@ -34,10 +45,6 @@ def all_patients(request):
     }
     return HttpResponse(template.render(context, request))
 
-def add_patient(request, data):
-    patient = Patient(data[['user_id ']], data['password'], data['first_name'],  data['last_name'], data['email'], data['date_of_birth'], data['phone'], data['address'], data['gender'], data['Type'], data['admin'], data['curp'], data['insurance'], data['allergies'])
-    patient.save()
-    return HttpResponse(patient)
 
 def update_patient(request, data):
     for key in data:
@@ -47,28 +54,31 @@ def update_patient(request, data):
 
     return HttpResponse('Update patient goes here')
 
-def delete_patient(request, user_id):
-    Patient.objects.filter(pk=user_id).delete()
-    return HttpResponse('Delete patient goes here')
 
 def all_dentists(request):
     if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        address = request.POST.get('address')
-        dob = request.POST.get('dob')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        specialty = request.POST.get('specialty')
-        cedula = request.POST.get('cedula')
-        rate = request.POST.get('hourly_rate')
-        
+        if 'first_name' in request.POST:
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            address = request.POST.get('address')
+            dob = request.POST.get('dob')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            specialty = request.POST.get('specialty')
+            cedula = request.POST.get('cedula')
+            rate = request.POST.get('hourly_rate')
 
-        latest_dentist = Dentist.objects.latest('user_id')
-        latest_dentist_id = latest_dentist.user_id
-        
-        dentist = Dentist((latest_dentist_id + 1), first_name, last_name, email, dob, phone, address, False, rate, cedula, specialty)
-        dentist.save()
+            try:
+                latest_dentist = Dentist.objects.latest('user_id')
+                latest_dentist_id = latest_dentist.user_id
+            except:
+                latest_dentist_id = 0
+
+            dentist = Dentist((latest_dentist_id + 1), first_name, last_name, email, dob, phone, address, False, rate, cedula, specialty)
+            dentist.save()
+
+        if 'delete' in request.POST:
+            Dentist.objects.filter(pk=request.POST.get('delete')).delete()
 
     dentists_list = Dentist.objects.all()
     template = loader.get_template('dentists.html')
@@ -89,26 +99,29 @@ def update_dentist(request, data):
         dentist.save()
     return HttpResponse('Update Dentist goes here')
 
-def delete_dentist(request, user_id):
-    Dentist.objects.filter(pk=user_id).delete()
-    return HttpResponse('Deleted successfully')
-
 def all_staff(request):
     if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        address = request.POST.get('address')
-        dob = request.POST.get('dob')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        salary = request.POST.get('salary')
-        staff_type = request.POST.get('staff_type')
+        if 'first_name' in request.POST:
+                first_name = request.POST.get('first_name')
+                last_name = request.POST.get('last_name')
+                address = request.POST.get('address')
+                dob = request.POST.get('dob')
+                email = request.POST.get('email')
+                phone = request.POST.get('phone')
+                salary = request.POST.get('salary')
+                staff_type = request.POST.get('staff_type')
 
-        latest_staff = Staff.objects.latest('user_id')
-        latest_staff_id = latest_staff.user_id
-        
-        staff = Staff((latest_staff_id + 1), first_name, last_name, email, dob, phone, address, False, salary, staff_type)
-        staff.save()
+                try:
+                    latest_staff = Staff.objects.latest('user_id')
+                    latest_staff_id = latest_staff.user_id
+                except:
+                    latest_staff_id = 0
+
+                staff = Staff((latest_staff_id + 1), first_name, last_name, email, dob, phone, address, False, salary, staff_type)
+                staff.save()
+
+        if 'delete' in request.POST:
+            Staff.objects.filter(pk=request.POST.get('delete')).delete()
 
     staff_list = Staff.objects.all()
     template = loader.get_template('staff.html')
@@ -117,11 +130,6 @@ def all_staff(request):
     }
     return HttpResponse(template.render(context, request))
 
-def add_staff(request, data):
-    staff = Staff(data[['user_id ']], data['password'], data['first_name'],  data['last_name'], data['email'], data['date_of_birth'], data['phone'], data['address'], data['gender'], data['Type'], data['admin'], data['salary'], data['staff_type'])
-    staff.save()
-    return HttpResponse(staff)
-
 def update_staff(request, data):
     for key in data:
         staff = Staff.objects.get(data['user_id'])
@@ -129,23 +137,32 @@ def update_staff(request, data):
         staff.save()
     return HttpResponse('Update Dentist goes here')
 
-def delete_staff(request, user_id):
-    Patient.objects.filter(pk=user_id).delete()
-    return HttpResponse('Deleted successfully')
 
 def all_appointments(request):
     if request.method == 'POST':
-        patient_id = request.POST.get('patient_id')
-        dentist_id = request.POST.get('dentist_id')
-        date = request.POST.get('date')
-        status = request.POST.get('status')
-        description = request.POST.get('description')
+        if 'patient_id' in request.POST:
+            patient_id = request.POST.get('patient_id')
+            dentist_id = request.POST.get('dentist_id')
+            date = request.POST.get('date')
+            status = request.POST.get('status')
+            description = request.POST.get('description')
 
-        latest_appointment = Appointment.objects.latest('appointment_id')
-        latest_appointment_id = latest_appointment.appointment_id
+            try:
+                latest_appointment = Appointment.objects.latest('appointment_id')
+                latest_appointment_id = latest_appointment.appointment_id
+            except:
+                latest_appointment_id = 0
+
+            appointment = Appointment((latest_appointment_id + 1), patient_id, dentist_id, date, status, description)
+
+            try:
+                appointment.save()
+            except IntegrityError as e:
+                return HttpResponse('Error: Paciente o Médico no existente')
+
         
-        appointment = Appointment((latest_appointment_id + 1), patient_id, dentist_id, date, status, description)
-        appointment.save()
+        if 'delete' in request.POST:
+            Appointment.objects.filter(pk=int(request.POST.get('delete'))).delete()
 
     appointments_list = Appointment.objects.all()
     template = loader.get_template('appointments.html')
@@ -172,18 +189,29 @@ def delete_appointment(request, appointment_id):
 
 def all_treatments(request):
     if request.method == 'POST':
-        appointment_id = request.POST.get('appointment_id')
-        start_date = request.POST.get('start_date')
-        end_date = request.POST.get('end_date')
-        diagnostic = request.POST.get('diagnostic')
-        medication = request.POST.get('medication')
-        observations = request.POST.get('observations')
+        if 'appointment_id' in request.POST:
+            appointment_id = request.POST.get('appointment_id')
+            start_date = request.POST.get('start_date')
+            end_date = request.POST.get('end_date')
+            diagnostic = request.POST.get('diagnostic')
+            medication = request.POST.get('medication')
+            observations = request.POST.get('observations')
 
-        latest_treatment = Treatment.objects.latest('treatment_id')
-        latest_treatment_id = latest_treatment.appointment_id
+            try:
+                latest_treatment = Treatment.objects.latest('treatment_id')
+                latest_treatment_id = latest_treatment.appointment_id
+            except:
+                latest_treatment_id = 0
         
-        treatment = Treatment((latest_treatment_id + 1), appointment_id, start_date, end_date, diagnostic, medication, observations)
-        treatment.save()
+            treatment = Treatment((latest_treatment_id + 1), appointment_id, start_date, end_date, diagnostic, medication, observations)
+            try:
+                treatment.save()
+            except IntegrityError as e:
+                return HttpResponse('Error: cita no existente')
+        
+        if 'delete' in request.POST:
+            Treatment.objects.filter(pk=request.POST.get('delete')).delete()
+
 
     treatments_list = Treatment.objects.all()
     template = loader.get_template('treatments.html')
